94cb6d9566840a1be30aaf12991972989490e4ad
jjluke <jj.luke808@gmail.com>
2016-01-15 17:02:18 -0800

    Update non-baryonic-bobo.py

--- non-baryonic-bobo.py
+++ non-baryonic-bobo.py
@@ -1,2 +1,9 @@
 print " big bouncing penguin"
 print " atom->baryon->quark"
+bobo=input " hello"
+if bobo = "hello":
+  bobobo=input "hello"
+  if bobobo = "hello":
+    bobobobo= input "hello"
+else:
+  print "bye bye"
